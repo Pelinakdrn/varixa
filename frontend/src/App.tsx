fad19252ad47b41d1390a935119d7bfae01bfe19
Pelin akduran <pelinakduran@gmail.com>
2025-05-08 20:31:05 +0300

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/Welcomep";
 import AboutUs from "./pages/AboutUs";
+import BlogPage from "./pages/blog"; 
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/About" element={<AboutUs />} /> 
+        <Route path="/Blog" element={<BlogPage />} /> 
       </Routes>
     </Router>
   );
