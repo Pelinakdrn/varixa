25554392592dac6dccdcd06936e93d574332fc4d
Pelin akduran <pelinakduran@gmail.com>
2025-05-14 19:00:07 +0300

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import fileRoutes from "./routes/file.routes";
 import authRoutes from "./routes/auth.routes";
-import predictRoutes from "./routes/predict.routes"; // ✅ EKLENDİ
 
 const app = express();
 
@@ -11,7 +10,6 @@ app.use(express.json());
 
 app.use("/api/file", fileRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/predict", predictRoutes); // ✅ EKLENDİ
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
